7e7ddc32add094497ff7637d873ae6e1afa80b9a
Artem Golovko <artem.golovko@siemens.com>
2022-01-08 10:58:32 +0300

    add notifications

--- app/src/main/java/com/example/mooncalendar/activities/calendar/CalendarActivity.kt
+++ app/src/main/java/com/example/mooncalendar/activities/calendar/CalendarActivity.kt
@@ -1,16 +1,25 @@
 package com.example.mooncalendar.activities.calendar
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
 import androidx.core.view.children
+import com.example.mooncalendar.R
 import com.example.mooncalendar.databinding.CalendarActivityBinding
-import com.example.mooncalendar.utils.DateChangedNotifier
-import com.example.mooncalendar.utils.MoonPhaseCalculator
-import com.example.mooncalendar.utils.TimeZoneChangedNotifier
+import com.example.mooncalendar.utils.*
 import net.time4j.android.ApplicationStarter
 import java.time.DayOfWeek
+import java.time.LocalDate
 import java.time.YearMonth
+import java.time.ZoneId
 import java.time.format.TextStyle
 import java.time.temporal.WeekFields
 import java.util.*
@@ -19,6 +28,8 @@ class CalendarActivity : AppCompatActivity() {
 
     private companion object {
         const val CALENDAR_RANGE_YEARS: Long = 20
+        const val NOTIFICATION_CHANNEL_ID = "notifications"
+        const val NOTIFICATION_ID = 0
     }
 
     private lateinit var binding: CalendarActivityBinding
@@ -38,6 +49,7 @@ class CalendarActivity : AppCompatActivity() {
 
         val daysOfWeek = setupDaysLegend()
         setupCalendar(daysOfWeek.first(), CALENDAR_RANGE_YEARS)
+        setupNotifications()
         setupDateChangedListener()
         setupTimeZoneChangedListener()
     }
@@ -98,13 +110,77 @@ class CalendarActivity : AppCompatActivity() {
         binding.calendarView.setup(firstMonth, lastMonth, firstDayOfWeek)
     }
 
+    private fun setupNotifications() {
+        binding.notificationSwitch.isChecked = false
+        binding.notificationMoonPhases.isEnabled = false
+        binding.notificationMoonPhases.adapter = ArrayAdapter(
+            baseContext,
+            R.layout.support_simple_spinner_dropdown_item,
+            MoonPhase.values().map { it.toString() }
+        )
+
+        binding.settingsButton.setOnClickListener {
+            val visible = binding.notificationPanel.visibility
+            if (visible == View.VISIBLE) {
+                binding.notificationPanel.visibility = View.GONE
+            } else {
+                binding.notificationPanel.visibility = View.VISIBLE
+            }
+        }
+        binding.notificationSwitch.setOnCheckedChangeListener { _, isChecked ->
+            binding.notificationMoonPhases.isEnabled = isChecked
+        }
+        createNotificationChannel()
+    }
+
+    private fun createNotificationChannel() {
+        val notificationChannel = NotificationChannel(
+            NOTIFICATION_CHANNEL_ID,
+            NOTIFICATION_CHANNEL_ID,
+            NotificationManager.IMPORTANCE_HIGH
+        )
+        val notificationManager =
+            this.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(notificationChannel)
+    }
+
     private fun setupDateChangedListener() {
         registerReceiver(dateChangedNotifier, dateChangedNotifier.getIntentFilter())
         dateChangedNotifier.subscribe {
             binding.calendarView.notifyDateChanged(it.before)
             binding.calendarView.notifyDateChanged(it.after)
             binding.calendarView.scrollToDate(it.after)
+            handleNotification(it)
         }
+    }
+
+    private fun handleNotification(event: BroadcastEventsNotifier.ChangedEvent<LocalDate>) {
+        val enabled = binding.notificationSwitch.isChecked
+        if (!enabled) return
+
+        val stateView = binding.notificationMoonPhases
+        val state = stateView.getItemAtPosition(stateView.selectedItemPosition).toString()
+            .let { MoonPhase.valueOf(it) }
+        val phase = moonPhaseCalculator.getPhase(event.after, ZoneId.systemDefault())
+        if (state == phase) {
+            showNotification(phase)
+        }
+    }
+
+    private fun showNotification(moonPhase: MoonPhase) {
+        val resultIntent = Intent(this, CalendarActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(
+            this, 0, resultIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+        val notification = NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
+            .setSmallIcon(moonPhase.imageId)
+            .setContentTitle("Moon phase happened: $moonPhase")
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(pendingIntent)
+            .build()
+        (getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager)
+            .notify(NOTIFICATION_ID, notification)
     }
 
     private fun setupTimeZoneChangedListener() {
